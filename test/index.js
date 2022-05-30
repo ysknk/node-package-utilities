d3149e9b503ca6139db803f42fe7c3b7a4f502b6
ysknk <31568245+ysknk@users.noreply.github.com>
2022-05-30 14:58:53 +0900

    Update files

--- test/index.js
+++ test/index.js
@@ -6,15 +6,15 @@ const sleep = msec => new Promise(resolve => setTimeout(resolve, msec))
   utils.message.begin()
 
   // a
-  await sleep(300)
+  await sleep(500)
   utils.message.success('a')
 
   // b
-  await sleep(300)
+  await sleep(500)
   utils.message.success('b')
 
   // c
-  await sleep(300)
+  await sleep(500)
   utils.message.failure('c')
 
   utils.message.finish('test')
