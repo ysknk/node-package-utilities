04485fd2143ecf84723b11770d71ce93fe45e12a
ysknk <31568245+ysknk@users.noreply.github.com>
2022-07-26 17:49:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import * as color from './lib/color/index.js'
 import * as message from './lib/message/index.js'
 import * as time from './lib/time/index.js'
 import * as value from './lib/value/index.js'
+import * as file from './lib/file/index.js'
 
 export default {
   color,
   message,
   time,
-  value
+  value,
+  file
 }
 
